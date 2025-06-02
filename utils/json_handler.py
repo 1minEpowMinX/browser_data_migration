79496eb15d18489a2ac3658772a179be908a1852
1minEpowMinX <nobusymaile@gmail.com>
2025-06-02 11:59:38 +0300

    Improve JSON structure for browser session data and add documentation for functions

--- utils/json_handler.py
+++ utils/json_handler.py
@@ -5,21 +5,38 @@ from typing import Any
 
 
 def create_default_json() -> dict:
+    """Creates a default JSON structure for browser session data.
+
+    Returns:
+        dict: The default JSON structure.
+    """
     return {
         "timestamp": datetime.now().isoformat(),
         "browsers": {
-            "Firefox": {"running": False, "tabs": {}},
-            "Chrome": {"running": False, "tabs": {}},
-            "Edge": {"running": False, "tabs": {}},
+            "Firefox": {"running": False, "tabs": [], "path": r""},
+            "Chrome": {"running": False, "tabs": [], "path": r""},
+            "Edge": {"running": False, "tabs": [], "path": r""},
         },
     }
 
 
 def save_to_json(data: Any, filename: Path | str) -> None:
+    """Saves the given data to a JSON file.
+
+    Args:
+        data (Any): The data to save.
+        filename (Path | str): The path to the JSON file.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
 
 def load_from_json(filename: Path | str) -> Any:
+    """Loads data from a JSON file.
+
+    Args:
+        filename (Path | str): The path to the JSON file.
+    """
+
     with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
