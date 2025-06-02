9411f752c1ff2bb5d0ea4ec5d39629ffd49bf4d5
1minEpowMinX <nobusymaile@gmail.com>
2025-06-02 15:47:48 +0300

    Simplifying browser data export: merging data retrieval functions for Chrome, Edge and Firefox into one, improving browser profile handling.

--- migrations/exporter.py
+++ migrations/exporter.py
@@ -1,12 +1,44 @@
+from typing import Optional
+
 from utils.check_browser_status import is_browser_running, kill_browser_process
-from utils.browser_paths import (
-    get_chrome_profile_path,
-    get_edge_profile_path,
-    get_firefox_profiles_path,
-)
+from utils.browser_paths import get_browser_profile_path
 from utils.json_handler import create_default_json, save_to_json
 from session_parsers.chromium_parser import find_latest_snss_file, parse_snss_file
 from session_parsers.firefox_parser import find_latest_recovery_file, parse_jsonlz4_file
+
+
+def tab_to_dict(tab) -> Optional[dict]:
+    # Безопасно получаем url и title текущей вкладки
+    if tab.entries and 0 <= tab.index < len(tab.entries):
+        entry = tab.entries[tab.index]
+        return {
+            "url": getattr(entry, "url", ""),
+            "title": getattr(entry, "title", ""),
+        }
+    return None
+
+
+def get_browser_data(json: dict, browser: str) -> None:
+    browser_path = get_browser_profile_path(browser)
+    json["browsers"][browser]["path"] = browser_path
+    if not browser_path:
+        return
+
+    if browser == "Firefox":
+        recovery_file = find_latest_recovery_file(browser_path)
+        if recovery_file:
+            firefox_windows = parse_jsonlz4_file(recovery_file)
+            tabs = [
+                tab_to_dict(tab) for window in firefox_windows for tab in window.tabs
+            ]
+            # Убираем None, если вкладка невалидна
+            json["browsers"]["Firefox"]["tabs"] = [t for t in tabs if t]
+    else:
+        snss_file = find_latest_snss_file(browser_path)
+        if snss_file:
+            browser_windows = parse_snss_file(snss_file)
+            tabs = [tab_to_dict(tab) for tab in browser_windows.tabs]
+            json["browsers"][browser]["tabs"] = [t for t in tabs if t]
 
 
 def browser_data_export():
@@ -20,56 +52,6 @@ def browser_data_export():
                 f"[!] {browser} запущен, завершаем процесс для безопасного экспорта..."
             )
             kill_browser_process(browser)
-
-    chrome_path = get_chrome_profile_path()
-    json["browsers"]["Chrome"]["path"] = chrome_path
-    if chrome_path:
-        snss_file = find_latest_snss_file(chrome_path)
-        if snss_file:
-            chrome_windows = parse_snss_file(snss_file)
-            json["browsers"]["Chrome"]["tabs"] = [
-                {
-                    "url": tab.entries[tab.index].url,
-                    "title": tab.entries[tab.index].title,
-                }
-                for tab in chrome_windows.tabs
-                if tab.entries
-                and 0
-                <= tab.index
-                < len(
-                    tab.entries
-                )  # TODO: Make sure the tab index is set correctly in session_parsers/chromium_parser.py
-            ]
-
-    edge_path = get_edge_profile_path()
-    json["browsers"]["Edge"]["path"] = edge_path
-    if edge_path:
-        snss_file = find_latest_snss_file(edge_path)
-        if snss_file:
-            edge_windows = parse_snss_file(snss_file)
-            json["browsers"]["Edge"]["tabs"] = [
-                {
-                    "url": tab.entries[tab.index].url,
-                    "title": tab.entries[tab.index].title,
-                }
-                for tab in edge_windows.tabs
-                if tab.entries and 0 <= tab.index < len(tab.entries)
-            ]
-
-    firefox_path = get_firefox_profiles_path()
-    json["browsers"]["Firefox"]["path"] = firefox_path
-    if firefox_path:
-        recovery_file = find_latest_recovery_file(firefox_path)
-        if recovery_file:
-            firefox_windows = parse_jsonlz4_file(recovery_file)
-            json["browsers"]["Firefox"]["tabs"] = [
-                {
-                    "url": tab.entries[tab.index].url,
-                    "title": tab.entries[tab.index].title,
-                }
-                for window in firefox_windows
-                for tab in window.tabs
-                if tab.entries and 0 <= tab.index < len(tab.entries)
-            ]
+            get_browser_data(json, browser)
 
     save_to_json(json, "browser_sessions.json")
