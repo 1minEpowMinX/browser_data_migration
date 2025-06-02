9e05c65976b7725dd12039248334240372c9914b
1minEpowMinX <nobusymaile@gmail.com>
2025-06-02 17:12:18 +0300

    Refactor JSON handler: enhance browser data structure and improve error handling in save/load functions

--- utils/json_handler.py
+++ utils/json_handler.py
@@ -1,7 +1,6 @@
 import json
 from datetime import datetime
 from pathlib import Path
-from typing import Any
 
 
 def create_default_json() -> dict:
@@ -13,30 +12,83 @@ def create_default_json() -> dict:
     return {
         "timestamp": datetime.now().isoformat(),
         "browsers": {
-            "Firefox": {"running": False, "tabs": [], "path": r""},
-            "Chrome": {"running": False, "tabs": [], "path": r""},
-            "Edge": {"running": False, "tabs": [], "path": r""},
+            "Firefox": {
+                "running": False,
+                "tabs": [],
+                "path": r"",
+                "executable": {
+                    "Windows": [
+                        r"C:\Program Files\Mozilla Firefox\firefox.exe",
+                        r"C:\Program Files (x86)\Mozilla Firefox\firefox.exe",
+                    ],
+                    "Linux": [r"/usr/bin/firefox"],
+                },
+            },
+            "Chrome": {
+                "running": False,
+                "tabs": [],
+                "path": r"",
+                "executable": {
+                    "Windows": [
+                        r"C:\Program Files\Google\Chrome\Application\chrome.exe",
+                        r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",
+                    ],
+                    "Linux": [r"/usr/bin/google-chrome", r"/usr/bin/chromium-browser"],
+                },
+            },
+            "Edge": {
+                "running": False,
+                "tabs": [],
+                "path": r"",
+                "executable": {
+                    "Windows": [
+                        r"C:\Program Files\Microsoft\Edge\Application\msedge.exe",
+                        r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
+                    ],
+                    "Linux": [r"/usr/bin/microsoft-edge", r"/usr/bin/msedge"],
+                },
+            },
         },
     }
 
 
-def save_to_json(data: Any, filename: Path | str) -> None:
+def save_to_json(data: dict, filename: Path | str) -> None:
     """Saves the given data to a JSON file.
 
     Args:
-        data (Any): The data to save.
+        data (dict): The data to save.
         filename (Path | str): The path to the JSON file.
+
+    Raises:
+        ValueError: If the specified file is not a JSON file.
     """
+
+    if not str(filename).endswith(".json"):
+        raise ValueError(f"File '{filename}' is not a JSON file.")
+
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
 
-def load_from_json(filename: Path | str) -> Any:
+def load_from_json(filename: Path | str) -> dict:
     """Loads data from a JSON file.
 
     Args:
         filename (Path | str): The path to the JSON file.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        ValueError: If the specified file is not a JSON file.
+
+    Returns:
+        dict: The data loaded from the JSON file.
     """
+
+    if not Path(filename).exists():
+        raise FileNotFoundError(f"File '{filename}' does not exist.")
+
+    if not str(filename).endswith(".json"):
+        raise ValueError(f"File '{filename}' is not a JSON file.")
 
     with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
