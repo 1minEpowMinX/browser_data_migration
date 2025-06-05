aea2f332abdcfe86f1d26f26de8ac98ed0f1f1a4
1minEpowMinX <nobusymaile@gmail.com>
2025-06-05 17:26:41 +0300

    Remove the StreamHandler handler from the logging configuration

--- utils/logger.py
+++ utils/logger.py
@@ -1,4 +1,4 @@
-from logging import getLogger, basicConfig, FileHandler, StreamHandler, INFO
+from logging import getLogger, basicConfig, FileHandler, INFO
 from pathlib import Path
 
 LOG_FILE = Path("browser_data_migration.log")
@@ -8,7 +8,6 @@ basicConfig(
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
     handlers=[
         FileHandler(LOG_FILE, encoding="utf-8"),
-        StreamHandler(),  # to see logs in console
     ],
 )
 
